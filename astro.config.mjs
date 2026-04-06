ca674a17a3a8744ad10c5f1b4108f0c99479bcf9
divyatak <divya090389@gmail.com>
2026-04-07 01:31:43 +0530

    Switch to custom domain divyatak.art, remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://divyatak.github.io',
-  base: '/divyapersonalwebsite',
+  site: 'https://divyatak.art',
 })
